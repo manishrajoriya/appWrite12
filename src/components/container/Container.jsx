0ffa9cf480397f503213324f1820681c03e2aab9
manish <gkrajoriya3@gmail.com>
2024-06-21 07:21:26 +0530

    lateruploaded

---
+++ src/components/container/Container.jsx
@@ -0,0 +1,9 @@
+import React, { Children } from 'react'
+
+function Container() {
+  return <div className='w-full px-4 mx-auto max-w-7xl'>
+        {Children}</div>;
+  
+}
+
+export default Container
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Logo from "../Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import Container from "./container/Container";
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}
